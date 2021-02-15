f7c969e30b012acd451c09b9a36f9468ae90edf6
Dan <dan.esposito@ynov.com>
2021-02-15 20:39:55 +0100

    create user and admin in bdd (fixture)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user
+            ->setEmail("admin@admin.fr")
+            ->setRoles(["ROLE_ADMIN"])
+            ->setPassword($this->passwordEncoder->encodePassword($user, "admin"))
+        ;
+        $manager->persist($user);
+        $user2 = new User();
+        $user2->setEmail("user@user.fr")
+            ->setPassword($this->passwordEncoder->encodePassword($user2, "user"))
+        ;
 
+        $manager->persist($user2);
         $manager->flush();
     }
 }
