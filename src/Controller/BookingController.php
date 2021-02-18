b4d09c95fbda7ef7e5f00a6e5c8eeef4d7f9f07e
Dan <dan.esposito@ynov.com>
2021-02-18 01:05:13 +0100

    Route for All Bookings

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\BookingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookingController extends AbstractController
+{
+    /**
+     * @Route("/api/booking", name="booking", methods="get")
+     * @param BookingRepository $bookingsRepository
+     * @return JsonResponse
+     */
+    public function index(BookingRepository $bookingsRepository): JsonResponse
+    {
+        return $this->json($booking = $bookingsRepository->findAll(), 200, [],["groups" => "booking"]);
+    }
+}
